e7dc5bd7d7f10148e35bc8eadc160fda7671ed41
Guilherme Costa <132716901+gu1costa@users.noreply.github.com>
2025-03-08 10:02:05 -0300

    Implementing CRUD of tasks.

--- app.py
+++ app.py
@@ -1,20 +1,86 @@
-from flask import Flask   #classe do flask usada para criar a aplicação
+from flask import Flask, request, jsonify #classe do flask usada para criar a aplicação
+from models.task import Task
 
-#__name__ = "__main__"  quando executado de forma manual.
 app = Flask(__name__) 
 
-#rota com os clientes, permitindo receber informações e devolve-las para quem está socilitando
-@app.route("/")  #método route. parâmetro endpoint. "/" representa a rota inicial/local.
-def hello_world():    #o que va ser executado ao acessar a rota.
-    return "Hello world!"
+#CRUD
+#CRUD = Create, Read, Update and Delete.
+#Tabela: tarefa.
 
-@app.route("/about")
-def about():
-    return "Página sobre."
+tasks = []
+task_id_control = 1
+
+#Create
+@app.route("/tasks", methods=['POST']) #methos define o método HTTP
+def create_task():
+    global task_id_control #define o valor da variável dentro da função.
+    data = request.get_json() #o request vem do Flask. o get_json recupera os dados inseridos pelo cliente. o data vira um dicionário com esses métodos
+    print(data)
+    new_task = Task(id= task_id_control, title=data["title"], description=data.get("description", ""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso."}) #jsonify vem do flask que transforma o conteúdo de um dicionário em json ou xml. as normas da API REST exigem que retorne json/xml
+
+#Read
+@app.route("/tasks", methods=['GET'])
+def get_tasks():
+    task_list = [] #não é possível utilizar a variável "task = []" do início do código por que ela está armazenando objetos da classe Task
+
+    for task in tasks:
+        task_list.append(task.to_dict()) #o to_dict retorna a task que está na lista como está no formato da classe Task.
+
+    #forma alternativa: task_list = [task.to_dict() for task in tasks]
+
+    output = {
+                "tasks": task_list,
+                "total_tasks": len(task_list)
+            }
+    return jsonify(output)
+
+@app.route("/tasks/<int:id>", methods=["GET"]) #parâmetro: id do tipo inteiro. permite receber uma variável do usuário.
+def get_task(id):
+    task = None
+    for t in tasks: #"t" é cada atividade dentro da lista tasks
+        if t.id == id:
+            return jsonify(t.to_dict())
+    return jsonify({"message": "Não foi possível encontrar a atividade."}), 404 #404 significa não encontrado.
+
+#Update
+@app.route("/tasks/<int:id>", methods=["PUT"])
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+        
+    print(task)
+
+    if task == None:
+        return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+    
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+    print(task)
+    return jsonify({"message": "Tarefa atualizada com sucesso."})
+
+#Delete
+@app.route("/tasks/<int:id>", methods=["DELETE"])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break #quebra o loop para não percorrer todas as verificações.
+
+    if not task: #se não encontrar nenhuma task
+        return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+        
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa deletada com sucesso."})
 
 if __name__ == "__main__":
-    app.run(debug=True)   #método run com a propriedade debug como parâmetro. o debug ajuda  a  viualizar informações sobre o que está acontecendo no servidor web. recomendado apenas para desenvolvimento local.
-
-#rotas são a ponte de acesso com outros programas ou usuários que estão acessando determinada informação.
-
-#registro de requisição: ip / dia e horário / protocolo (get/http) / código da requisição.
+    app.run(debug=True)   
